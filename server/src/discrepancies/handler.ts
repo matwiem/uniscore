f3cbf6a0a65803359d1b2ceaba4f47b735490c5f
Michał Matwiejczuk <michal.matwiejczuk@lingarogroup.com>
2023-02-06 22:05:43 +0100

    feat: handle invalid query parameters

--- server/src/discrepancies/handler.ts
+++ server/src/discrepancies/handler.ts
@@ -5,6 +5,7 @@ import {
 } from 'express'
 
 import { DiscrepanciesService, Filters } from '@src/discrepancies/service'
+import { validationResult } from 'express-validator'
 
 type RequestQuery = Filters
 
@@ -12,7 +13,7 @@ export type QueryParam<P extends keyof RequestQuery> = keyof Pick<RequestQuery,P
 
 export interface DiscrepanciesHandler {
     get: (
-        request: Request<unknown, unknown, unknown, RequestQuery>,
+        request: Request<Record<string,never>, unknown, unknown, RequestQuery>,
         response: Response,
         next: NextFunction
     ) => Promise<void>
@@ -22,6 +23,10 @@ export const makeHandler = (service: DiscrepanciesService): DiscrepanciesHandler
     return {
         get: async (req, res, next) => {
             try {
+                const errors = validationResult(req)
+                if (!errors.isEmpty()) {
+                    res.status(400).json({errors: errors.array()})
+                }
                 const filters = req.query
                 res.json(await service.discrepancies(filters))
             } catch (error: unknown) {
