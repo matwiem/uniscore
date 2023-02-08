9c99255fad2fcf00ca07c44c98d0ccec1e453827
Michał Matwiejczuk <michal.matwiejczuk@lingarogroup.com>
2023-02-08 08:51:44 +0100

    feat(app providers): router, react-query, chakra-ui

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import { App } from './App'
 import reportWebVitals from './reportWebVitals'
-import { ChakraProvider } from '@chakra-ui/react'
+import { AppProviders } from './AppProviders'
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 )
 root.render(
     <React.StrictMode>
-        <ChakraProvider>
+        <AppProviders>
             <App />
-        </ChakraProvider>
+        </AppProviders>
     </React.StrictMode>
 )
 
