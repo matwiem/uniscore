d4cf48f27a711de419313cc812cb7c09a0a07634
Michał Matwiejczuk <michal.matwiejczuk@lingarogroup.com>
2023-02-08 09:56:30 +0100

    chore(ui): reorganize structure

---
+++ client/src/components/ui/organisms/DiscrepanciesList.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+
+import { DiscrepanciesList } from './DiscrepanciesList'
+import { discrepanciesJSON } from '../../../api/discrepancies.mock'
+
+test('renders \'no discrepancies \' when empty array given', () => {
+    render(<DiscrepanciesList discrepancies={[]} />)
+
+    const message = screen.getByText(/no discrepancies/i)
+
+    expect(message).toBeInTheDocument()
+})
+
+test('renders all discrepancies', () => {
+    render(<DiscrepanciesList discrepancies={discrepanciesJSON} />)
+
+    const items = screen.getAllByTestId('discrepancy-card')
+
+    expect(items).toHaveLength(discrepanciesJSON.length)
+})
