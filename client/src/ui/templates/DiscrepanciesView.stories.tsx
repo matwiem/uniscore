3fc3cecf8fb5415f2a93661e19cd6ee34f435fb7
Michał Matwiejczuk <michal.matwiejczuk@lingarogroup.com>
2023-02-10 10:43:51 +0100

    fix: wrap components with MemoryRouter

--- client/src/ui/templates/DiscrepanciesView.stories.tsx
+++ client/src/ui/templates/DiscrepanciesView.stories.tsx
@@ -1,11 +1,12 @@
 import { type ComponentMeta, type ComponentStory } from '@storybook/react'
 import { DiscrepanciesView } from './DiscrepanciesView'
 import { discrepanciesJSON } from '../../api/discrepancies.mock'
+import { MemoryRouter } from 'react-router-dom'
 
 const componentMeta: ComponentMeta<typeof DiscrepanciesView> = {
     component: DiscrepanciesView
 }
 
-export const GenericDiscrepanciesView: ComponentStory<typeof DiscrepanciesView> = () => <DiscrepanciesView discrepancies={discrepanciesJSON} heading={'All Discrepancies'} />
+export const GenericDiscrepanciesView: ComponentStory<typeof DiscrepanciesView> = () => <MemoryRouter><DiscrepanciesView discrepancies={discrepanciesJSON} heading={'All Discrepancies'} /></MemoryRouter>
 
 export default componentMeta
